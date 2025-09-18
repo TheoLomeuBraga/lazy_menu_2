c3c4fa56e8d68d5f829b4152342002dcd8e40d84
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-09-18 16:00:12 -0300

    add slider int display suport

--- addons/lazy_menu_2/lazy_settings/elements/slider/no_display.gd
+++ addons/lazy_menu_2/lazy_settings/elements/slider/no_display.gd
@@ -1,6 +1,9 @@
 extends Label
 
-
+@export var show_only_int : bool = true
 
 func _process(delta: float) -> void:
-	text = str($"../VBoxContainer/HSlider".value)
+	if show_only_int:
+		text = str(int($"../VBoxContainer/HSlider".value))
+	else:
+		text = str($"../VBoxContainer/HSlider".value)
